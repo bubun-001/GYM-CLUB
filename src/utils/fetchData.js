1ede1eaa0b3f717a9ad9bfa9194d6b1c654683ad
bubun-001 <b420032@iiit-bh.ac.in>
2023-07-18 19:26:30 +0530

    GYM-CLUB

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '4b6cfae911msha514eeb3e199f9fp1a8cf7jsndb6df8552a91',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
